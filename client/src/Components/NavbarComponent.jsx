814ed17412431c49187e7b211907612b01602ca9
avgfx15 <avgfx15@gmail.com>
2025-10-01 00:10:47 +0530

    Basic Setup done

---
+++ client/src/Components/NavbarComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// & Navbar Component
+const NavbarComponent = () => {
+  // ^ Render Navbar Component
+  return <div>NavbarComponent</div>;
+};
+
+export default NavbarComponent;
